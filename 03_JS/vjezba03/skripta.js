3e6e5f0ddcfe83b2130b1f4e1bf5b27d2f690ab3
bosutsko <bosutsko@gmail.com>
2026-07-20 19:00:45 +0200

    vježba

---
+++ 03_JS/vjezba03/skripta.js
@@ -0,0 +1,71 @@
+
+
+//ULAZ
+
+const a = 7 //parseInt(prompt('Unesi prvi broj'))
+const b = 3 //parseInt(prompt('Unesi drugi broj'))
+
+//ALGORITAM
+
+let rez = a + b
+
+//IZLAZ
+
+console.log(a, '+', b, '=', rez)
+
+console.log(`${a} + ${b} = ${rez}`)
+
+//modulo operator (%)
+
+// ostatak nakon cjelobrojnog djeljenja
+
+// 9 % 2
+// 4 * 2 = 8
+// 9 - 8 = 1 - rezultat
+
+console.log(9 % 2) // 1
+console.log(8 % 2) // 0
+
+
+
+rez = rez + 2
+
+console.log(rez)
+
+rez += 2
+
+console.log(rez)
+
+
+let i = 0 
+
+i = i + 1
+
+i += 1
+
+i++
+
+console.log(i)
+
+i = 0
+
+console.log(i++)
+
+console.log(++i)
+
+i = 2
+let j = 1
+i = j++ - i
+j += --i - ++j
+console.log(i - j)
+
+
+console.log('5' != 4)
+console.log(5 != 5)
+console.log('5' !== 5)
+
+const uvjet = '5' !== '5'
+
+console.log('uvjet', typeof uvjet, uvjet)
+
+
